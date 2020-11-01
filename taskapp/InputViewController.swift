50534618e6204fc85e7f2aa3b6d97eae5cf85f05
tetelab <tetelabtt@gmail.com>
2020-11-01 23:51:53 +0900

    追加

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 松岡哲平 on 2020/10/26.
+//  Copyright © 2020 tetelab. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
