cfcdecceefb80e47e2d908bd88cb866a1332cac5
tetelab <tetelabtt@gmail.com>
2020-11-11 22:32:57 +0900

    絞り込み機能の追加

---
+++ Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  
+//
+//  Created by 松岡哲平 on 2020/11/01.
+//
+
+import Foundation
---
+++ taskapp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 松岡哲平 on 2020/11/01.
+//  Copyright © 2020 tetelab. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var category = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
